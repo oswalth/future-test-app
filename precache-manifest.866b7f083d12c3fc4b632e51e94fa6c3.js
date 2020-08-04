86e65db84a696c8bc3a27b09b7d26849ea413062
oswalth <oswalth2@gmail.com>
2020-08-04 13:02:50 +0300

    Updates

---
+++ precache-manifest.866b7f083d12c3fc4b632e51e94fa6c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceec0a00a622d8c6bfe39694f72b4e92",
+    "url": "/future-test-app/index.html"
+  },
+  {
+    "revision": "62d940546967fee28992",
+    "url": "/future-test-app/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "21a0e1faaa6439d0a1df",
+    "url": "/future-test-app/static/css/main.b26b074d.chunk.css"
+  },
+  {
+    "revision": "62d940546967fee28992",
+    "url": "/future-test-app/static/js/2.12936841.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/future-test-app/static/js/2.12936841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21a0e1faaa6439d0a1df",
+    "url": "/future-test-app/static/js/main.de2559d1.chunk.js"
+  },
+  {
+    "revision": "ac3df84bfea2b2a04760",
+    "url": "/future-test-app/static/js/runtime-main.1ef470bc.js"
+  }
+]);
